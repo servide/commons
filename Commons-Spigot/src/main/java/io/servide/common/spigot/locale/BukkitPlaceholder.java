935a01a13efcce7b010e9068149cac8580b1ed48
BuildTools <unconfigured@null.spigotmc.org>
2018-02-19 20:16:21 -0500

    removed useless packages

--- Commons-Spigot/src/main/java/io/servide/common/spigot/locale/BukkitPlaceholder.java
+++
@@ -1,8 +0,0 @@
-package io.servide.common.spigot.locale;
-
-import io.servide.common.locale.PlaceholderMessagePart;
-import org.bukkit.command.CommandSender;
-
-public interface BukkitPlaceholder extends PlaceholderMessagePart<CommandSender> {
-
-}
